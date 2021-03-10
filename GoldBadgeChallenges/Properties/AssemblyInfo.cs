9064659ff066cad53803ce4b7b5dc435cd39596a
peterkline1150 <peterkline1150@gmail.com>
2021-03-10 15:24:45 -0600

    Add project files.

---
+++ GoldBadgeChallenges/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoldBadgeChallenges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoldBadgeChallenges")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4b8b4267-d573-41ab-998c-b746d7d18513")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GoldBadgeChallenges/WeekOneDayThree.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GoldBadgeChallenges
+{
+    [TestClass]
+    public class WeekOneDayThree
+    {
+        string longWord = "Supercalifragilisticexpialidocious";
+        [TestMethod]
+        public void ChallengeDayThreePart1()
+        {
+            foreach (char letter in longWord)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+        [TestMethod]
+        public void ChallengeDayThreePart2()
+        {
+            foreach (char letter in longWord)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                }
+                else
+                {
+                    Console.WriteLine("Not an i");
+                }
+            }
+        }
+        [TestMethod]
+        public void ChallengeDayThreeBonus1()
+        {
+            Console.WriteLine(longWord.Length);
+        }
+        [TestMethod]
+        public void ChallengeDayThreeBonus2()
+        {
+            foreach (char letter in longWord)
+            {
+                switch (letter)
+                {
+                    case 'i':
+                        Console.WriteLine(letter);
+                        break;
+                    case 'l':
+                        Console.WriteLine('L');
+                        break;
+                    default:
+                        Console.WriteLine("Not an i or l");
+                        break;
+                }
+            }
+        }
+    }
+}
