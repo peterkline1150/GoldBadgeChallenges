dc9c33983f58c6b8735784c32a55cc5a5010cb34
peterkline1150 <peterkline1150@gmail.com>
2021-03-11 15:34:45 -0600

    W1D5 Challenges

---
+++ MorningChallengeW1D5/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MorningChallengeW1D5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MorningChallengeW1D5")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a76164ba-2b27-46d4-bf45-ebd575c1e88d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MorningChallengeW1D5/User.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MorningChallengeW1D5
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; }
+        public DateTime BirthDate { get; set; }
+        public User()
+        {
+
+        }
+        public User(string firstName, string lastName, int id, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            ID = id;
+            BirthDate = birthDate;
+        }
+        public string GetFullName()
+        {
+            string fullName = $"{FirstName} {LastName}";
+            return fullName;
+        }
+        public int AgeOfUserInYears()
+        {
+            TimeSpan ageSpan = DateTime.Now - BirthDate;
+            double ageInYears = ageSpan.TotalDays / 365.25;
+            int age = Convert.ToInt32(Math.Floor(ageInYears));
+            return age;
+        }
+
+    }
+}
